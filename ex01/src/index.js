23e3efd51964d02b73e2c0229e7bbaaef3f6719e
arminpeco <apeco1906@gmail.com>
2021-09-29 12:54:58 +0200

    Added file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+const array1=['MON','TUE','WED','THU','FRI'];
+function main(arr) {
+    // only change code below this line
+        let array2=[...arr];
+
+
+    // only change code below this line
+    return array2;
+}
+console.log(main(array1));
+module.exports=main;
